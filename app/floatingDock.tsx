396dce76f31438918e298f4e7465c45ffbf31ffa
Pranav Soni <pranavsoni1101@gmail.com>
2025-06-21 15:54:42 +0530

    A basic setup using next js and aceternity ui

---
+++ app/floatingDock.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { FloatingDock } from "@/components/ui/floating-dock";
+import {
+  IconBrain,
+  IconBuildingSkyscraper,
+  IconAddressBook,
+  IconBriefcase,
+} from "@tabler/icons-react";
+
+export function FloatingDockDemo() {
+  const links = [
+    {
+      title: "About",
+      icon: (
+        <IconAddressBook className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+
+    {
+      title: "Experience",
+      icon: (
+        <IconBuildingSkyscraper className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Projects",
+      icon: (
+        <IconBriefcase className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Skills",
+      icon: (
+        <IconBrain className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+  ];
+  return (
+    <div className="flex items-center justify-center h-[35rem] w-full">
+      <FloatingDock
+        mobileClassName="translate-y-20" // only for demo, remove for production
+        items={links}
+      />
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from 'react';
+import { FloatingDockDemo } from './floatingDock';
+
+const Home = () => {
+  return(
+    <>
+      <h1>Hello World</h1>
+      <FloatingDockDemo />
+    </>
+  )
+}
+
+export default Home;
